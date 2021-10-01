040899d679e183b81d55582fd5ef3cb228e45410
Azar <azarovvit@mail.ru>
2021-10-01 03:09:06 +0000

    3

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask.helpers import url_for
 from extensions import app, db, render_template, redirect
 
+list_chapter=['Programming','Sport','Traiding']
+
+
 @app.route("/")
 def index():
     return redirect(url_for('home'))
@@ -15,7 +18,21 @@ def login():
 
 @app.route("/register")
 def register():
-    return render_template('register.html')    
+    return render_template('register.html')
+
+@app.route("/library")
+def library():
+    return render_template('programming/library.html')       
+
+@app.route("/content1")
+def content1():
+    return render_template('programming/content1.html')
+
+@app.route("/resources")
+def resources():
+    return render_template('programming/resources.html')
 
 if __name__=="__main__":
     app.run(debug=True)
+
+     
